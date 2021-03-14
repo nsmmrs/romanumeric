fd581347f7832ff31d694a203822c4e344604d2d
Noah Summers <ns.commits@gmail.com>
2021-03-13 19:25:42 -0600

    simplify alcotest setup

--- test/suites/utils.ml
+++ test/suites/utils.ml
@@ -1,6 +1,3 @@
-open Alcotest
-open Romanum
-
 (** Test suite for the Utils module. *)
 
 let test_hello_with_name name () =
@@ -12,3 +9,5 @@ let suite =
   [ "can greet Tom", `Quick, test_hello_with_name "Tom"
   ; "can greet John", `Quick, test_hello_with_name "John"
   ]
+
+let () = Alcotest.run "romanum" [ "Utils", suite ]
