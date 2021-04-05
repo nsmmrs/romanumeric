53218e21650a455805b146d7240e67aca207a257
Noah Summers <ns.commits@gmail.com>
2021-04-05 09:08:30 -0500

    add filter_take helper

--- lib/utils.ml
+++ lib/utils.ml
@@ -1,3 +1,17 @@
 let greet name = "Hello " ^ name ^ "!"
 
 let%test "can greet Ash" = String.equal (greet "Ash") "Hello Ash!"
+
+let filter_take ~f:valid n list =
+  let rec take_from ?(taken = []) ?(n_taken = 0) list =
+    if n_taken = n then taken
+    else
+      match list with
+      | [] -> taken
+      | e :: remaining ->
+          let taken, n_taken =
+            if valid e then (e :: taken, n_taken + 1) else (taken, n_taken)
+          in
+          take_from remaining ~taken ~n_taken
+  in
+  take_from list |> List.rev
